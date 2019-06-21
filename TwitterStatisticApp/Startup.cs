9303d1fae8fe6f162997e355ef132b7797c1197d
RafaelTamura <37556028+RafaelTamura@users.noreply.github.com>
2019-06-21 13:27:40 -0300

    Add files via upload

---
+++ TwitterStatisticApp/Startup.cs
@@ -0,0 +1,155 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+using System;
+using TwitterStatisticApp.Application.Twitter;
+using TwitterStatisticApp.Application.Twitter.Interface;
+using TwitterStatisticApp.Infra.Data.Repository;
+using TwitterStatisticApp.Infra.Data.Repository.Interface;
+
+namespace TwitterStatisticApp
+{
+    public class Startup
+    {
+        private string CaminhoXmlComments { get; set; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+
+            CaminhoXmlComments = string.Format("{0}{1}.xml", AppDomain.CurrentDomain.BaseDirectory, env.ApplicationName);
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            #region Cors Configuration
+            services.AddCors(options =>
+                {
+                    options.AddPolicy("AllowAll",
+                        builder =>
+                        {
+                            builder
+                            .AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader()
+                            .AllowCredentials();
+                        });
+                });
+            #endregion
+
+            #region MVC Configuration
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2); 
+            #endregion
+
+            #region Dependency Injection
+            services.AddSingleton<IConfiguration>(Configuration);
+            services.AddSingleton<ITwitterApp, TwitterApp>();
+            services.AddSingleton<ILanguageRepository, LanguageRepository>();
+            services.AddSingleton<ITweetRepository, TweetRepository>();
+            services.AddSingleton<IUserRepository, UserRepository>();
+            services.AddSingleton<ITweetsByHourRepository, TweetsByHourRepository>();
+            services.AddSingleton<ITweetsByTagRepository, TweetsByTagRepository>();
+            #endregion
+
+            #region Spa Angular Configuration
+            // In production, the Angular files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/dist";
+            }); 
+            #endregion
+
+            #region AutoMapper
+            services.AddAutoMapper(); 
+            #endregion
+
+            #region Swagger Configuration
+            services.AddSwaggerGen(c =>
+                {
+                    c.SwaggerDoc("v1.000",
+                        new Info
+                        {
+                            Title = "API",
+                            Version = "v1.000",
+                            Description = "Serviços do Sistema",
+                            Contact = new Contact
+                            {
+                                Name = "Teste"
+                            }
+                        });
+
+                    c.CustomSchemaIds(x => x.FullName);
+
+                    c.IncludeXmlComments(CaminhoXmlComments);
+                }); 
+            #endregion
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseSpaStaticFiles();
+
+            #region Cors
+            app.UseCors("AllowAll");
+            #endregion
+
+            #region MVC Routes
+            app.UseMvc(routes =>
+                {
+                    routes.MapRoute(
+                        name: "default",
+                        template: "{controller}/{action=Index}/{id?}");
+                });
+            #endregion
+
+            #region Spa Angular
+            app.UseSpa(spa =>
+                {
+                // To learn more about options for serving an Angular SPA from ASP.NET Core,
+                // see https://go.microsoft.com/fwlink/?linkid=864501
+
+                spa.Options.SourcePath = "ClientApp";
+
+                    if (env.IsDevelopment())
+                    {
+                        spa.UseAngularCliServer(npmScript: "start");
+                    }
+                }); 
+            #endregion
+
+            #region Swagger
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1.000/swagger.json",
+                    "Serviços do Sistema");
+            }); 
+            #endregion
+        }
+    }
+}
